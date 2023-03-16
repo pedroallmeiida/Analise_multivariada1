c47551364c690b42af973cda0e93d7d2c83d6a92
giovanadesouza <127260013+Giovana-de-Souza@users.noreply.github.com>
2023-03-16 15:01:25 -0300

    Rename Atividade 1, Multivariada 1.R to Atividade 1, Multivariada - Giovana de Souza1.R

--- Atividades/Atividade 1, Multivariada - Giovana de Souza1.R
+++ Atividades/Atividade 1, Multivariada - Giovana de Souza1.R
@@ -5,8 +5,8 @@ x_1 = c(34,26,82,5) #vetor x1
 x_1
 x_2 = c(55.5,47,105,7.5) #vetor x2
 x_2
-mean_x1 = mean(x_1) #Média de x1
-mean_x2 = mean(x_2) #Média de x2
+mean_x1 = mean(x_1) #MÃ©dia de x1
+mean_x2 = mean(x_2) #MÃ©dia de x2
 mean_x1
 mean_x2
 n_1=length(x_1) #tamanho do vetor x1
@@ -32,11 +32,11 @@ n2 =length(x2)
 n2
 n3 =length(x3)
 n3
-mean_x1= mean(x1) #Média x1
+mean_x1= mean(x1) #MÃ©dia x1
 mean_x1
-mean_x2= mean(x2) #Média x2
+mean_x2= mean(x2) #MÃ©dia x2
 mean_x2
-mean_x3= mean(x3) #média x3
+mean_x3= mean(x3) #mÃ©dia x3
 mean_x3
 arrays_x = c(mean_x1,mean_x2,mean_x3) #vetor de medias
 arrays_x
@@ -52,15 +52,15 @@ cov_x2_x3
 matrixcov =matrix(c(var(x1),cov(x1,x2),cov(x1,x3),cov(x1,x2),var(x2),cov(x2,x3),
                     cov(x1,x3),cov(x2,x3),var(x3)),nrow = 3,ncol = 3,byrow = TRUE)
 matrixcov  #Matrix de covariancia
-cor(x1,x2) #corelação de x1 com x2
-cor(x1,x3) #corelação de x1 com x3
-cor(x2,x3) #corelação de x2 com x3
-cor(x1,x1) #corelação de x1 com x1
-cor(x2,x2) #corelação de x2 com x2
-cor(x3,x3) #corelação de x3 com x3
+cor(x1,x2) #corelaÃ§Ã£o de x1 com x2
+cor(x1,x3) #corelaÃ§Ã£o de x1 com x3
+cor(x2,x3) #corelaÃ§Ã£o de x2 com x3
+cor(x1,x1) #corelaÃ§Ã£o de x1 com x1
+cor(x2,x2) #corelaÃ§Ã£o de x2 com x2
+cor(x3,x3) #corelaÃ§Ã£o de x3 com x3
 matrixcor = matrix(c(cor(x1,x1),cor(x1,x2),cor(x1,x3),cor(x1,x2),cor(x2,x2),
                      cor(x2,x3),cor(x1,x3),cor(x2,x3),cor(x3,x3)),nrow = 3,ncol = 3, byrow = TRUE)
-matrixcor #matrix de corelação
+matrixcor #matrix de corelaÃ§Ã£o
 
 
  
