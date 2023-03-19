1ff1a07175226d1731f0e6e6319492f0462f36cf
jheymisson13 <127241002+jheymisson13@users.noreply.github.com>
2023-03-19 20:48:40 -0300

    Add files via upload

---
+++ Atividades/Atividade 2 de análise multivariada 1,jheymisson.R
@@ -0,0 +1,82 @@
+#Questão 1
+
+
+x1 <- c(3,4,2,6,8,2,5)
+x2 <-c(5,5.5,4,7,10,5,7.5)
+
+#medias
+mean(x1)
+mean(x2)
+#Variancias
+var(x1)
+var(x2)
+
+#Covariancia 
+cov(x1,x2)
+
+#Distancia euclidiana 
+m <- sqrt(sum((x1-x2)^2))
+m
+distancia <- dist(rbind(x1, x2), method = "euclidean")
+distancia
+
+#Distancia estatística 
+a = sd(x1)
+b = sd(x2)
+j <- (x1)/(a)
+j
+k <- (x2)/(b)
+k
+sqrt(sum((j-k)^2))
+
+#Questão 2
+
+y1 = c(1,2,3,4,5,6,7,8,9,11)
+y2 = c(18.95,19,17.95,15.54,14,12.95,8.94,7.49,6,3.99)
+#Gráfico de dispersão
+plot(y1,y1,ylab = "PreÃ§o de vendas em milhares de dÃ³lares", xlab = "Tempo de FabricaÃ§Ã£o em anos")
+
+#Inferir o sinal da amostra de covariÃ¢ncia
+cov(y1,y2)
+
+#Médias e variancias amostrais
+mean(y1)
+mean(y2)
+var(y1)
+var(y2)
+
+#Covariancia e coeficiente de correlação
+W <-matrix(c(y1,y2),nrow = 10,ncol = 2)
+cov(W)
+cor(W)
+
+#Correlação deu negativa e bastante proxima de -1, o que significa que quanto mais o tempo de fabricaÃ§Ã£o cresce, mais o preÃ§o de venda diminui.
+
+#Vetor de matriz de médias
+WA = c(mean(y1),mean(y2))
+WA
+#Matriz de covariancia
+WB = cov(m)
+WB
+#Matriz de correlaÃ§Ã£o
+WT = cor(m)
+WT
+
+#QuestÃ£o 3
+
+B1 = c(9,2,6,5,8)
+B2 = c(12,8,6,4,10)
+B3 = c(3,4,0,2,1)
+
+#Vetor de médias 
+D = c(mean(w1),mean(w2),mean(w3))
+D
+#Matriz de covariancia
+D2 = matrix(c(w1,w2,w3),nrow = 5, ncol = 3)
+D3
+D4 = cov(wd)
+D4
+#Matriz de correlação
+A1 = cor(wd)
+A1
+
